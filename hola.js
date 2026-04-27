791a5aac3e3746209bffd16ebde66cf9ce865df2
Carlo <Carlosmora292005@gmail.com>
2026-04-27 14:58:45 -0600

    feat: Ejercicios logicos de Javascript

--- hola.js
+++ hola.js
@@ -19,7 +19,7 @@ console.log("Precio unitario: " + precio);
 console.log("Cantidad unitaria: " + cantidad);
 console.log("Total a pagar: " + total);
 
-
+---------------------------------------------------------------------
 
 let nota = 62;
 if (nota >= 70){
@@ -31,6 +31,7 @@ else{
     console.log("Reprobado")
 }
 
+---------------------------------------------------------------------
 
 function evaluarnota(nota){
    if (nota >= 70){
@@ -47,12 +48,13 @@ evaluarnota(80)
 evaluarnota(62)
 evaluarnota(50)
 
-
+----------------------------------------------------------------------
 
 for(let i = 1; i<=5; i++){
     console.log("El numero es: " + i)
 }
 
+----------------------------------------------------------------------
 
 let i = 1
 
@@ -61,6 +63,7 @@ while(i <= 5){
     i++
 }
 
+----------------------------------------------------------------------
 
 let notas = [30, 45, 80, 75, 90];
 
@@ -80,6 +83,7 @@ else{
 
 evaluarnotas(notas)
 
+-------------------------------------------------------------------------
 
 let precios= [1500, 2000, 800, 3500, 1200];
 let total = 0;
@@ -90,7 +94,7 @@ for (let i = 0; i < 5; i++){
 
     console.log("El monto total es: " + total)
 
-*/
+---------------------------------------------------------------------------
     
 let salarios = [250000, 800000, 450000, 1200000, 150000];
 
@@ -111,8 +115,91 @@ for(i = 0; i < salarios.length; i++){
     Calificarsalarios(salarios)
 
 
-/*
 Mayor o igual a 800,000 → "Salario Alto"
 Mayor o igual a 400,000 → "Salario Medio"
 Menor a 400,000 → "Salario Bajo"
+
+-----------------------------------------------------------------
+
+Creá un programa que tenga una lista de 5 productos con sus precios y que muestre:
+
+El nombre del producto
+Si es Caro (más de 10,000), Moderado (entre 5,000 y 10,000) o Barato (menos de 5,000)
+
+
+let productos = ["Arroz", "Televisor", "Café", "Celular", "Pan"];
+let precios = [1200, 85000, 2500, 45000, 800];
+function MostrarPP(productos, precios){
+for(i = 0; i < productos.length; i++){
+if(precios[i] > 10000){
+    console.log("El " + productos[i] + " es Caro")
+} else if(precios[i] >= 5000){
+    console.log("El " + productos[i] + " es de precio moderado")
+}else{
+    console.log("El " + productos[i] + " es barato")
+}
+}
+}
+
+MostrarPP(productos, precios)
+
+-------------------------------------------------------------------
+
+Sumá todas las notas con un for
+Dividí entre la cantidad de notas para sacar el promedio
+Si el promedio es mayor o igual a 70 → Aprobó, si no → Reprobó
+
+let notas = [85, 62, 45, 70, 58];
+function SumarNotas(notas){
+    Total = 0;
+
+
+    for(i = 0; i < notas.length; i++){
+        Total += notas[i]
+    }
+
+        let Promedio = Total / notas.length 
+
+
+        if(Promedio >= 70){
+            console.log("Aprobo")
+        }else{
+            console.log("Reprobo")
+        }
+    } 
+
+SumarNotas(notas)
+
+---------------------------------------------------------------------
+
+let temperaturas = [22, 35, 18, 40, 28, 31];
+
+function encontrarMayor(temperaturas) {
+    let mayor = temperaturas[0];
+
+    for (let i = 0; i < temperaturas.length; i++) {
+        if (temperaturas[i] > mayor) {
+            mayor = temperaturas[i];
+        }
+    }
+
+    console.log("La temperatura más alta fue: " + mayor);
+}
+
+encontrarMayor(temperaturas);
+
+------------------------------------------------------------------------
 */
+
+let numeros = [4, 7, 12, 9, 20];
+Pares = 0
+Impares = 0
+for(i = 0; i < numeros.length; i++){
+    if(numeros[i] % 2 === 0){
+        ++Pares
+    }else{
+        ++Impares
+    }
+}
+console.log("Son pares: " + Pares)
+console.log("Son impares: " + Impares)
